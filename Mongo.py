e0e06e69cac00eff7f4259aa781184fad22566ce
mishariveli <74241906+mishariveli@users.noreply.github.com>
2024-09-17 16:07:14 +0300

    Update Mongo.py

--- Mongo.py
+++ Mongo.py
@@ -7,7 +7,8 @@ conn_params = {
     'collection': 'employees'  # Name of your MongoDB collection
 }
 
-first_name = 'John'
+# Define the first name
+first_name = 'Khaled'
 
 try:
     # Establish a connection to the MongoDB database
@@ -18,15 +19,15 @@ try:
     db = client[conn_params['database']]
     collection = db[conn_params['collection']]
 
-    # Define the query to find the document with the specified name
-    query = {"name": name}
+    # Define the query to find the document with the specified first name
+    query = {"first_name": first_name}
     document = collection.find_one(query)
 
     if document:
-        # Print the ID, name, and salary of the document
-        print(f"ID: {document.get('_id')}, Name: {document.get('name')}, Salary: {document.get('salary')}")
+        # Print the ID, first name, and salary of the document
+        print(f"ID: {document.get('_id')}, First Name: {document.get('first_name')}, Salary: {document.get('salary')}")
     else:
-        print(f"No record found with {name}")
+        print(f"No record found with First Name: {first_name}")
 
 except Exception as e:
     print(f"An error occurred: {e}")
