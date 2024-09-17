dfd89bc098ba02290f6e013e6cd8020f6faefd81
mishariveli <74241906+mishariveli@users.noreply.github.com>
2024-09-17 16:15:16 +0300

    Update Mongo.py

--- Mongo.py
+++ Mongo.py
@@ -5,11 +5,9 @@ conn_params = {
     'host': 'mongodb://192.168.200.86:27017/',  # MongoDB URI
     'database': 'creative',  # Name of your MongoDB database
     'collection': 'employees'  # Name of your MongoDB collection
-
 }
 
-# Define the first name
-first_name = 'Mark'
+first_name = 'Marl'
 
 try:
     # Establish a connection to the MongoDB database
@@ -28,11 +26,9 @@ try:
         # Print the ID, first name, and salary of the document
         print(f"ID: {document.get('_id')}, First Name: {document.get('first_name')}, Salary: {document.get('salary')}")
     else:
-        print(f"No record found with First Name: {first_name}")
+        print(f"No record found with first name {first_name}")
 
 except Exception as e:
     print(f"An error occurred: {e}")
 
-finally:
-    # Close the connection
-    client.close()
+fina
