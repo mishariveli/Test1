50ffb1c2fb23c1582c6d41400716e25fbfdfbd54
mishariveli <74241906+mishariveli@users.noreply.github.com>
2024-09-17 14:17:15 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,14 @@
 # hello_world.py
 print("Hello, World! new 92101010")
+
 import psycopg2
 from psycopg2 import sql, Error
 
 def fetch_user_salary(username):
+    connection = None
     try:
-        connection = mysql.connector.connect(
+        # Establish a connection to the PostgreSQL database
+        connection = psycopg2.connect(
             host='192.168.200.86',
             user='postgre',
             password='123456789',
@@ -13,15 +16,15 @@ def fetch_user_salary(username):
             port='5432'
         )
 
-        if connection.is_connected():
+        if connection is not None:
             print("Connected to the database")
 
             cursor = connection.cursor()
 
-        
+            # Execute query to fetch salary
             query = "SELECT salary FROM users WHERE username = %s"
             cursor.execute(query, (username,))
-          
+
             result = cursor.fetchone()
 
             if result:
@@ -33,11 +36,10 @@ def fetch_user_salary(username):
         print(f"Error: {e}")
 
     finally:
-        if connection.is_connected():
+        if connection is not None:
             cursor.close()
             connection.close()
             print("Database connection closed")
 
 if __name__ == "__main__":
-   
     fetch_user_salary("Ahmed")
